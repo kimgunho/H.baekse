be731eacef434e42bcdd05e9e570e97963f0ff22
kim <yuyukgh@naver.com>
2024-04-25 02:13:25 +0900

    footer 추가

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -5,7 +5,24 @@ import styles from './index.module.scss';
 const cx = classNames.bind(styles);
 
 const Footer = () => {
-  return <footer className={cx('container')}>footer</footer>;
+  return (
+    <footer className={cx('container')}>
+      <div className={cx('wrapper')}>
+        <div>
+          <p>
+            <span>울산본점 : 울산 북구 화봉로 74 건강백세 한의원</span>
+            <span>양산점 : 양산시 중앙로 178번지 1층 건강백세 한의원</span>
+          </p>
+          <p>
+            <span>사업자 등록번호 : 620-91-06937</span>
+            <span>대표원장 : 임정권</span>
+            <span>고객센터 : 052-289-4878</span>
+          </p>
+        </div>
+        <p>COPYRIGHT 2018 건강백세 한의원 ALL RIGHTS RESERVED.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
